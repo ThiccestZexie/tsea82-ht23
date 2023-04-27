75091289c7c679d2de714397ff3a8c9d1fcedd2e
KRATOS <samak519@student.liu.se>
2023-04-27 13:00:09 +0200

    Lab3 working we think.

--- LAB3/lab3/lab3/main.asm
+++ LAB3/lab3/lab3/main.asm
@@ -18,16 +18,13 @@
 .def tmpL=r18
 .def tmpH=r19
 
+
+
 ; --- SRAM
 .dseg
 .org SRAM_START
 TIME: .byte 4 ; time in BCD-format (BCD increments this)
 POS: .byte 1 ; which digit to display (MUX increments/reads this)
-
-; --- TABLES
-SEGTAB: 
-	.db $3F,$06,$5B,$4F,$66,$6D,$7D,$07,$7F,$6F
-
 
 ; --- CODE
 .cseg
@@ -39,21 +36,26 @@ jmp MUX ; INT0 interr.
 jmp BCD ; INT1 interr.
 .org INT_VECTORS_SIZE
 
+; --- TABLES
+SEGTAB: 
+	.db $3F,$06,$5B,$4F,$66,$6D,$7D,$07,$7F,$6F
+
 MAIN:
 MAIN_INIT:
 	; I/O init
 	ldi tmpL, $FF
-	out DDRA, tmpL ; PORTB is where segments will be lit
-	ldi tmpL, $00
-	out DDRD, tmpL ; PIND is where INT0 and INT1 will trigger from
+	out DDRA, tmpL ; PORTA is where segments will be lit
+	;ldi tmpL, $00
+	;out DDRD, tmpL ; PIND is where INT0 and INT1 will trigger from
 
 	; init SP
 	ldi tmpL,HIGH(RAMEND)
 	out SPH,tmpL
 	ldi tmpL,LOW(RAMEND)
 	out SPL,tmpL
+
 	; should interrupt on rising edge (both ISC0 and ISC1)
-	ldi tmpL,(1<<ISC01)|(0<<ISC00)|(1<<ISC11)|(0<<ISC10)
+	ldi tmpL,(1<<ISC01)|(1<<ISC00)|(1<<ISC11)|(1<<ISC10) ; THS MIGHT NOT OWORK ON PHYUSOCAL atmega16a
 	out MCUCR,tmpL
 	; enable ISC01 and ISC1
 	ldi tmpL,(1<<INT1)|(1<<INT0)
@@ -73,19 +75,33 @@ MUX_INIT:
 	push tmpL
 
 MUX_LOOP:
+	; tmpL loaded with POS variable
+	ldi tmpL, LOW(POS)
+	andi tmpL, 0b0011 ; mask away all bits above 2
 	; -- use POS to get current digit
 	; load X with TIME+POS
-	ldi tmpL, LOW(TIME+POS*8) ;tmpL now contains current digit
+	ldi XL, LOW(TIME)
+	ldi XH, HIGH(TIME)
+	add XL, tmpL
 
-	add tmpL, LOW(SEGTAB)
+	; tmpL loaded with current digit to display
+	ld tmpL, X
 
-	; -- convert it to hex segments
-	; load Z with SEGTAB + tmpL
-	; lpm tmpL, Z
+	; find how to displ sgemtns
+	ldi ZL, LOW(SEGTAB*2)
+	ldi ZH, HIGH(SEGTAB*2)
+	add ZL, tmpL
 
-	; out tmpL on PORTA
+	; tmpL loaded with encoded segment thingy
+	lpm tmpL, Z
 
-	; incr POS
+	; light up segments!
+	out PORTA, tmpL
+	
+	; increment POS in SRAM memory
+	ldi tmpL, LOW(POS)
+	inc tmpL
+	sts LOW(POS), tmpL
 
 MUX_EXIT:
 	; restore tmpL and flags from stack
@@ -96,11 +112,31 @@ MUX_EXIT:
 	reti
 
 BCD:
+BCD_INIT:
 	; save tmpL and flags on stack
+	push tmpL
 	in tmpL, SREG
 	push tmpL
-	;.. do something els
-
+	; load start of TIME variable
+	ldi XL, LOW(TIME)
+	ldi XH, HIGH(TIME)
+BCD_LOOP:
+	ld tmpL, X
+	inc tmpL
+	st X, tmpL
+	cpi tmpL, 10
+	brne BCD_EXIT
+	clr tmpL
+	st X+, tmpL
+	ld tmpL, X ; load ten-seconds
+	inc tmpL ; incr ten-seonds
+	st X, tmpL
+	cpi tmpL, 6
+	brne BCD_EXIT
+	clr tmpL
+	st X+, tmpL
+	jmp BCD_LOOP
+BCD_EXIT:
 	; restore tmpL and flags from stack
 	pop tmpL
 	out SREG, tmpL
